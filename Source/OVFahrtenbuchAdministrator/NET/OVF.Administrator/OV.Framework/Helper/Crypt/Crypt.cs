4e49b2e3989ea5a9942b572dbec0a77faa2804ff
xxDaxter <oliver.viete@gmx.de>
2020-02-17 22:27:38 +0100

    Framework umgebaut

---
+++ Source/OVFahrtenbuchAdministrator/NET/OVF.Administrator/OV.Framework/Helper/Crypt/Crypt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OV.Framework.Helper.Crypt
+{
+    public static class Crypt
+    {
+        public static string EncryptString(string password, string salt)
+        {
+            return null;
+        }
+    }
+}
---
+++ Source/OVFahrtenbuchAdministrator/NET/OVF.Administrator/OV.Framework/Helper/XML/XMLHelper.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using System.Xml.Linq;
+
+
+namespace OV.Framework.Helper.XML
+{
+    public class XMLHelper
+    {
+        #region "members"
+
+        #endregion
+
+        #region "methods"
+        #region "public"
+        public static void CreateMainConfigXML(string path, string server, string database, string user, string password, int port)
+        {
+            XElement xmlFile = new XElement("MainConfig",
+                new XElement("Server", server),
+                new XElement("database", database),
+                new XElement("user", user),
+                new XElement("password", password),
+                new XElement("port", port)
+                );
+        }
+
+        public static void CreateAdminConfigXML(string path, string password)
+        {
+            XElement xmlFile = new XElement("AdminConfig",
+                new XElement("Password", password)
+                );
+        }
+
+        public static bool CheckIfXmlExists(string path)
+        {
+            if (!File.Exists(path))
+            {
+                return false;
+            }
+
+            return true;
+        }
+        #endregion
+
+        #region "private"
+
+        #endregion
+        #endregion
+    }
+}
